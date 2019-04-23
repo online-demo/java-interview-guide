8bca754e9bef7010518bbb7ce6c20d1d28dd33ca
zhouguanya <zhouguanya@bestpay.com.cn>
2019-04-23 10:43:39 +0800

    项目初始化提交

---
+++ src/main/java/com/example/java/interview/guide/JavaInterviewGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.java.interview.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInterviewGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInterviewGuideApplication.class, args);
+    }
+
+}
