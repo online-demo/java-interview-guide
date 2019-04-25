ef33fab9d564d5de378b9c2335ffef07164e4749
zhouguanya <zhouguanya@bestpay.com.cn>
2019-04-25 20:12:38 +0800

    栈实现代码提交

---
+++ chapter1/src/main/java/com/example/java/interview/guide/chapter1/stack/link/LinkStack.java
@@ -0,0 +1,114 @@
+package com.example.java.interview.guide.chapter1.stack.link;
+
+import com.example.java.interview.guide.chapter1.stack.Stack;
+
+/**
+ * @Author : zhouguanya
+ * @Project : java-interview-guide
+ * @Date : 2019-04-25 16:34
+ * @Version : V1.0
+ * @Description :   链式栈
+ */
+public class LinkStack implements Stack {
+    /**
+     * 栈顶元素
+     */
+    private Node top;
+    /**
+     *  链式栈长度
+     */
+    private int size;
+    /**
+     * 链式栈构造器
+     */
+    public LinkStack() {
+        this.top = null;
+    }
+
+    /**
+     * 定义一个内部类Node，代表链式栈的节点
+     */
+    private class Node {
+        /**
+         * 保存链式栈的节点数据
+         */
+        private Object data;
+        /**
+         * 指向下个节点的引用
+         */
+        private Node next;
+        /**
+         * Node无参构造器
+         */
+        public Node() {
+
+        }
+        /**
+         * Node有参构造器
+         *
+         * @param element       链式栈节点数据
+         * @param next          链式栈下一个节点指针
+         */
+        public Node(Object element, Node next) {
+            this.data = element;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 栈是否为空
+     *
+     * @return true: 空  false: 非空
+     */
+    @Override
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    /**
+     * 入栈操作
+     *
+     * @param element 元素
+     * @return 入栈结果
+     */
+    @Override
+    public boolean push(Object element) {
+        if (element == null) {
+            throw new NullPointerException("入栈元素为空");
+        }
+        // 创建新Node节点，新节点指向原top节点
+        Node node = new Node(element, top);
+        // 修改top节点指向
+        top = node;
+        size++;
+        return true;
+    }
+
+    /**
+     * 出栈
+     *
+     * @return 元素
+     */
+    @Override
+    public Object pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        // 当前栈顶元素
+        Object element = top.data;
+        // 修改栈顶元素为原栈顶元素的下一个节点
+        top = top.next;
+        size--;
+        return element;
+    }
+
+    /**
+     * 栈的大小
+     *
+     * @return 栈中的元素个数
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+}
---
+++ chapter1/src/main/java/com/example/java/interview/guide/chapter1/stack/link/LinkStackDemo.java
@@ -0,0 +1,26 @@
+package com.example.java.interview.guide.chapter1.stack.link;
+
+/**
+ * @Author : zhouguanya
+ * @Project : java-interview-guide
+ * @Date : 2019-04-25 17:46
+ * @Version : V1.0
+ * @Description :   测试链式栈
+ */
+public class LinkStackDemo {
+    public static void main(String[] args) {
+        int size = 10;
+        LinkStack linkStack = new LinkStack();
+        for (int i = 0; i < size; i++) {
+            linkStack.push(i);
+        }
+        System.out.println("----------链式栈是否为空----------");
+        System.out.println(linkStack.isEmpty());
+        System.out.println("----------链式栈元素个数----------");
+        System.out.println(linkStack.size());
+        System.out.println("----------打印链式栈元素----------");
+        for (int i = 0; i < size; i++) {
+            System.out.print(linkStack.pop() + " ");
+        }
+    }
+}
