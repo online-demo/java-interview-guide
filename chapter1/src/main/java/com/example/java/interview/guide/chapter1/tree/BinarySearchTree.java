07acb9119025e018daa5fb38f9ebfb5fb3464bb2
zhouguanya <zhouguanya@bestpay.com.cn>
2019-05-05 14:08:33 +0800

    二叉查找树删除方法修改

--- chapter1/src/main/java/com/example/java/interview/guide/chapter1/tree/BinarySearchTree.java
+++ chapter1/src/main/java/com/example/java/interview/guide/chapter1/tree/BinarySearchTree.java
@@ -100,6 +100,11 @@ public class BinarySearchTree {
     /**
      * 从二叉查找树上删除指定元素
      *
+     * 删除元素分为以下情况：
+     * 情况1.待删除的节点没有左右子节点，可以直接删除
+     * 情况2.待删除的节点存在左子节点或者右子节点，删除后需要对子节点移动
+     * 情况3.待删除的节点存在左右2个子节点，可以通过和待删除的节点后继节点交换后转换为情况1或2
+     *
      * @param value     待删除元素
      * @return          删除结果
      */
@@ -108,101 +113,42 @@ public class BinarySearchTree {
         if (temp.data != value) {
             return false;
         }
-        // 删除的节点是叶子节点
-        if (temp.left == null && temp.right == null) {
-            // 如果删除的节点是根节点
-            if (temp == root) {
-                root = null;
-                // 如果删除的节点是父节点的右孩子，将父节点的右孩子置为空
-            } else if (temp.parent.data < temp.data) {
-                temp.parent.right = null;
-            } else {
-                // 如果删除的节点是父节点的左孩子，将父节点的左孩子置为空
-                temp.parent.left = null;
-            }
-            return true;
-            // 删除的节点有左右两个孩子
-        } else if (temp.left != null && temp.right != null) {
+        //首选处理第3种情况，删除的节点同时存在左右子节点
+        if (temp.left != null && temp.right != null) {
+            // 待删除节点的后继节点
             Node successor = findSuccessor(temp);
-            // 维护后继节点的左孩子的关系
-            successor.left = temp.left;
-            successor.left.parent = successor;
-            successor.right = temp.right;
-            successor.right.parent = successor;
-            // 因要把后继节点放在删除的节点位置上，所以需要处理后继节点的子节点
-            // 后继节点的有右孩子且后继节点的父节点不是当前删除的节点
-            if (successor.right != null && successor.parent != temp) {
-                // 后继节点的右孩子的父节点=后继节点的父节点
-                successor.right.parent = successor.parent;
-                // 后继节点的父节点的左孩子=后继节点的右孩子
-                successor.parent.left = successor.right;
-                // 后继节点的右孩子=删除节点的右孩子
-                successor.right = temp.right;
-                // 后继节点的右孩子的父节点=后继节点
-                successor.right.parent = successor;
-            } else {
-                successor.parent.left = null;
-            }
-            // 如果删除的是根节点
-            if (temp == root) {
-                // 后继节点的父节点为空
-                successor.parent = null;
-                // 更新根节点为后继节点
-                root = successor;
-                return true;
-            } else {
-                // 后继节点的父节点=删除节点的父节点
-                successor.parent = temp.parent;
-                // 删除节点的父节点小于删除节点的值：删除的是一个右节点
-                if (temp.parent.data < temp.data) {
-                    // 删除节点的右节点=后继节点
-                    temp.parent.right = successor;
-                } else {
-                    // 删除节点的左节点=后继节点
-                    temp.parent.left = successor;
-                }
-                return true;
-            }
-        } else {
-            // 删除的节点有一个孩子
-            // 删除的节点的右孩子非空
-            if (temp.right != null) {
-                // 如果删除的是根节点
-                if (temp == root) {
-                    root = root.right;
-                    return true;
-                }
-                temp.right.parent = temp.parent;
-                // 删除的是一个左节点
-                if (temp.data < temp.parent.data) {
-                    temp.parent.left = temp.right;
-                } else {
-                    // 删除的是一个右节点
-                    temp.parent.right = temp.right;
-                }
-                return true;
-            } else {
-                // 删除的节点有一个左孩子
-                // 删除的节点是根节点
-                if (temp == root) {
-                    root = temp.left;
-                    return true;
-                }
-                temp.left.parent = temp.parent;
-                // 删除的是一个左节点
-                if (temp.data < temp.parent.data) {
-                    temp.parent.left = temp.left;
-                } else {
-                    // 删除的是一个右节点
-                    temp.parent.right = temp.left;
-                }
-                return true;
-            }
+            // 转移待删除节点的后继节点值到当前节点
+            temp.data = successor.data;
+            // 把待删除的当前节点指向后继结点
+            temp = successor;
         }
+        //经过上一步处理，下面只有前2种情况，待删除的节点只有1个子节点或者没有节点
+        //不管待删除的节点是否有子节点，都获取待删除的节点的子节点
+        Node child;
+        // 待删除的节点的子节点
+        child = temp.left != null ? temp.left : temp.right;
+        if (child != null) {
+            // 将待删除子节点和待删除节点的父节点关联上
+            child.parent = temp.parent;
+        }
+        // 如果当前待删除的节点没有父节点（后继节点的情况到这儿时一定有父节点）
+        // 说明要待删除的就是根节点
+        if (temp.parent == null) {
+            root = child;
+        } else if (temp == temp.parent.left) {
+            //当前待删除的节点存在父节点，并且当前待删除的节点是其父节点的一个左节点
+            temp.parent.left = child;
+        } else if (temp == temp.parent.right) {
+            //当前待删除的节点存在父节点，并且当前待删除的节点是其父节点的一个右节点
+            temp.parent.right = child;
+        }
+        return true;
     }
 
     /**
      * 查询当前节点的后继节点
+     * 1.若当前节点没有右孩子，返回当前节点
+     * 2.若当前节点有右孩子，返回右子树中最小的大于当前节点的节点
      *
      * @param node  当前节点
      * @return      后继节点
