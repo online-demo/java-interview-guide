82878a393d96f1e943d44ec747bed0ab8fb8100c
zhouguanya <zhouguanya@bestpay.com.cn>
2019-07-04 18:50:17 +0800

    update

---
+++ chapter2/src/main/java/com/example/java/interview/guid/chapter2/array/maxcount/MostCountNumDemo.java
@@ -0,0 +1,20 @@
+package com.example.java.interview.guid.chapter2.array.maxcount;
+
+/**
+ * @Author : zhouguanya
+ * @Project : java-interview-guide
+ * @Date : 2019/7/4 18:00
+ * @Version : V1.0
+ * @Description : 测试求众数
+ */
+public class MostCountNumDemo {
+    public static void main(String[] args) {
+        MostCountNum mostCountNum = new MostCountNum();
+        int[] array1 = {3, 2, 3};
+        System.out.print("数组{3, 2, 3}的众数是");
+        System.out.println(mostCountNum.majorityNum(array1));
+        int[] array2 = {2, 2, 1, 1, 1, 2, 2};
+        System.out.print("数组{2,2,1,1,1,2,2}的众数是");
+        System.out.println(mostCountNum.majorityNum(array2));
+    }
+}
