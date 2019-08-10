5523266dd491166ed45e6385647837f7cb336478
Michael <michael@MichaeldeMacBook-Pro.local>
2019-08-10 17:01:39 +0800

    update

--- chapter2/src/main/java/com/example/java/interview/guide/chapter2/string/palindrome/PalindromeSplit.java
+++ chapter2/src/main/java/com/example/java/interview/guide/chapter2/string/palindrome/PalindromeSplit.java
@@ -8,7 +8,7 @@ import java.util.List;
  * @Project : java-interview-guide
  * @Date : 2019/6/26 14:44
  * @Version : V1.0
- * @Description : 分割回文串
+ * @Description : 分割回文字符串
  */
 public class PalindromeSplit {
     /**
@@ -19,53 +19,58 @@ public class PalindromeSplit {
     /**
      * 构造器
      *
-     * @param content   原字符串
+     * @param content 原字符串
      */
     public PalindromeSplit(String content) {
+        // 初始化原字符串
         this.content = content;
     }
+
     /**
-     * 结果
+     * 分割回文字符串的结果
      */
     List<List<String>> result = new ArrayList<>();
 
     /**
-     * 分割回文
+     * 分割回文字符串
      *
-     * @return      返回结果
+     * @return 返回结果
      */
     public List<List<String>> split() {
+        // 从某个位置搜索回文字符串
         search(0, new ArrayList<>());
+        // 返回分割回文字符串的结果
         return result;
     }
 
     /**
-     * 从某个位置搜索回文
+     * 从某个位置搜索回文字符串
      *
-     * @param index     位置
-     * @param list      回文
+     * @param index 位置
+     * @param list  回文字符串
      */
-    private void search(int index, List<String> list){
+    private void search(int index, List<String> list) {
         // 递归出口
-        if(index == content.length()){
+        if (index == content.length()) {
             // 到达递归出口，保存此次结果
             List<String> temp = new ArrayList<>(list);
             result.add(temp);
             return;
         }
-        // 从index位置向后查找回文
-        for(int i = index; i < content.length(); i++){
+        // 从index位置向后查找回文字符串
+        for (int i = index; i < content.length(); i++) {
             // 截取部分字符串
             String substring = content.substring(index, i + 1);
-            // 判断回文
+            // 使用判断回文字符串的方法用于验证substring是否是回文字符串
             Palindrome palindrome = new Palindrome();
-            // 是回文
-            if(palindrome.isPalindrome(substring)){
-                // 保存回文
+            // 如果substring是回文字符串
+            if (palindrome.isPalindrome(substring)) {
+                // 保存回文字符串
                 list.add(substring);
                 // 向后递归
                 search(i + 1, list);
-                // 递归出口返回后，删除list中的数据，供下一次外层循环保存回文
+                // 递归出口返回后，删除list中的数据
+                // 供下一次外层循环保存回文字符串
                 list.remove(list.size() - 1);
             }
         }
--- chapter2/src/main/java/com/example/java/interview/guide/chapter2/string/palindrome/PalindromeSplitDemo.java
+++ chapter2/src/main/java/com/example/java/interview/guide/chapter2/string/palindrome/PalindromeSplitDemo.java
@@ -7,7 +7,7 @@ import java.util.List;
  * @Project : java-interview-guide
  * @Date : 2019/6/26 16:24
  * @Version : V1.0
- * @Description : 分割回文串测试
+ * @Description : 分割回文字符串测试
  */
 public class PalindromeSplitDemo {
     public static void main(String[] args) {
@@ -15,7 +15,8 @@ public class PalindromeSplitDemo {
         PalindromeSplit palindromeSplit = new PalindromeSplit(content);
         // 分割回文
         List<List<String>> result = palindromeSplit.split();
-        System.out.println(content + "分割回文串结果：");
+        System.out.println(content + "分割回文字符串结果：");
+        // 打印测试结果
         for (List<String> list : result) {
             System.out.println(list);
         }
